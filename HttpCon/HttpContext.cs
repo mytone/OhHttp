651b79b92aff984848a4fe73e95245d278981709
zou_chao824 <zou_chao824@163.com>
2023-08-21 11:20:37 +0800

    index razor add

--- HttpCon/HttpContext.cs
+++ HttpCon/HttpContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,14 @@ namespace OhHttp.HttpCon
 {
     internal class HttpContext
     {
+
         public string url;
         public HttpContext(string _url)
         {
             url = _url;
         }
 
+        //module webclient get
         public string HttpGet()
         {
             using WebClient wc = new WebClient();
@@ -23,12 +26,14 @@ namespace OhHttp.HttpCon
             string result= wc.DownloadString(url);
             return result;
         }
-
+        //module webclient post
         private string httppost()
         {
             WebClient client = new WebClient();
+            
             //添加请求头
             client.Headers.Add("key", "value");  //key标头 //value标头值
+            
             //创建请求体
             NameValueCollection nvc = new NameValueCollection();
             nvc.Add("key1", "value1");
@@ -39,5 +44,26 @@ namespace OhHttp.HttpCon
             string str = System.Text.Encoding.UTF8.GetString(btys);
             return str;
         }
+
+
+        //module httpclient
+        public async Task<string> HttpClientGet()
+        {
+            using(HttpClient hc=new HttpClient())
+            {
+                var response=await hc.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string reshtml = await response.Content.ReadAsStringAsync();
+                    return reshtml;
+                }
+                else
+                {
+                    return "loading...";
+                }
+            }          
+        }
+
+
     }
 }
