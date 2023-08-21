e7c35266f69a2b8a96d2af34f8bb7caec1048fc8
zou_chao824 <zou_chao824@163.com>
2023-08-21 11:01:14 +0800

    httpcontext add2

--- HttpCon/HttpContext.cs
+++ HttpCon/HttpContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -21,6 +22,22 @@ namespace OhHttp.HttpCon
             wc.Encoding = Encoding.UTF8;
             string result= wc.DownloadString(url);
             return result;
+        }
+
+        private string httppost()
+        {
+            WebClient client = new WebClient();
+            //添加请求头
+            client.Headers.Add("key", "value");  //key标头 //value标头值
+            //创建请求体
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("key1", "value1");
+            nvc.Add("key2", "value2");
+            //提交数据
+            var btys = client.UploadValues(url, nvc);
+            //解析提交返回结果
+            string str = System.Text.Encoding.UTF8.GetString(btys);
+            return str;
         }
     }
 }
