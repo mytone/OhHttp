d67f8533891a6a813471503123ae3aeaa1588e6d
zou_chao824 <zou_chao824@163.com>
2023-08-21 09:19:22 +0800

    add httpcon/httpcontext

---
+++ HttpCon/HttpContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhHttp.HttpCon
+{
+    internal class HttpContext
+    {
+    }
+}
