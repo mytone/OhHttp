5fa56cded2cb69e409ad67599dd9c8ea53b6090a
zou_chao824 <zou_chao824@163.com>
2023-08-21 10:37:54 +0800

    httpcontext add2

--- HttpCon/HttpContext.cs
+++ HttpCon/HttpContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,14 @@ namespace OhHttp.HttpCon
         public HttpContext(string _url)
         {
             url = _url;
+        }
+
+        public string HttpGet()
+        {
+            using WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string result= wc.DownloadString(url);
+            return result;
         }
     }
 }
